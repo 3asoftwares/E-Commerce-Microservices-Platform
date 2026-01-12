cc0a12a8c46fc7a7b05b8cfee788bc631274136c
Abhishek Jain <abhishek.jain1@hp.com>
2026-01-12 22:58:47 +0530

    entry file

--- apps/shell-app/src/index.tsx
+++ apps/shell-app/src/index.tsx
@@ -1,13 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import '@3asoftwares/ui/styles.css';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Bootstrap pattern for Module Federation
+// This allows shared modules to be loaded before the app starts
+import('./bootstrap');
